c8a85a18ab9206a6abcd6b8b8e54a02ad11a4927
yuki9431 <yuki9431@gmail.com>
2022-03-13 22:21:51 +0900

    Change module source and filename

---
+++ oci_network.tf
@@ -0,0 +1,52 @@
+# oci_network
+locals {
+  next_hop_ids = {
+    "igw"   = module.oci_network.igw.id
+    "natgw" = module.oci_network.natgw.id
+  }
+}
+
+module "oci_network" {
+  source                 = "git@github.com:oracle-terraform-modules/terraform-oci-tdf-network.git"
+  default_compartment_id = var.default_compartment_id
+
+  vcn_options = {
+    display_name   = "terraform_vcn"
+    cidr           = "192.168.0.0/16"
+    enable_dns     = true
+    dns_label      = "terraformvcn"
+    compartment_id = null
+    defined_tags   = null
+    freeform_tags  = null
+  }
+
+  create_igw   = true
+  create_natgw = true
+
+  route_tables = {
+    private_systems = {
+      compartment_id = null
+      defined_tags   = null
+      freeform_tags  = null
+      route_rules = [
+        {
+          dst         = "0.0.0.0/0"
+          dst_type    = "CIDR_BLOCK"
+          next_hop_id = local.next_hop_ids["natgw"]
+        }
+      ]
+    },
+    public_systems = {
+      compartment_id = null
+      defined_tags   = null
+      freeform_tags  = null
+      route_rules = [
+        {
+          dst         = "0.0.0.0/0"
+          dst_type    = "CIDR_BLOCK"
+          next_hop_id = local.next_hop_ids["igw"]
+        }
+      ]
+    }
+  }
+}
