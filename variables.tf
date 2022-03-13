ea5674b5ff478d4715b37bdae9f6a5dea8edac53
yuki9431 <yuki9431@gmail.com>
2022-03-13 18:52:38 +0900

    new vcn作成まで確認済み

---
+++ variables.tf
@@ -0,0 +1,33 @@
+# provider identity parameters
+variable "region" {
+  # List of regions: https://docs.cloud.oracle.com/iaas/Content/General/Concepts/regions.htm#ServiceAvailabilityAcrossRegions
+  description = "the OCI region where resources will be created"
+  type        = string
+}
+
+# general oci parameters
+
+variable "compartment_id" {
+  type        = string
+  description = "compartment id where to create all resources"
+}
+
+variable "tenancy_ocid" {
+  type        = string
+  description = "tenancy id where to create all resources"
+}
+
+variable "user_ocid" {
+  type        = string
+  description = "user id where to access OCI API"
+}
+
+variable "fingerprint" {
+  type        = string
+  description = "fingerprint id where to access OCI API"
+}
+
+variable "private_key_path" {
+  type        = string
+  description = "private key path to to access OCI API"
+}
