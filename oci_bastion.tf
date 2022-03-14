186a0b99169a8c902b1f4276b2a9a1ca483b58ac
yuki9431 <yuki9431@gmail.com>
2022-03-14 14:03:19 +0900

    Add Bastion Service

---
+++ oci_bastion.tf
@@ -0,0 +1,20 @@
+module "oci_bastions" {
+  source = "git@github.com:yuki9431/terraform-oci-tdf-bastion-service.git"
+
+  default_compartment_id = var.default_compartment_id
+
+  bastions = {
+    bastion = {
+      bastion_type = "STANDARD"
+      compartment_id = null
+      target_subnet_id = module.oci_subnets.subnets.subnet_private.id
+
+      client_cidr_block_allow_list = ["0.0.0.0/0"]
+      defined_tags = null
+      freeform_tags = null
+      max_session_ttl_in_seconds    = "10800" # 180 minutes
+      phone_book_entry = null
+      static_jump_host_ip_addresses = null
+    }
+  }
+}
